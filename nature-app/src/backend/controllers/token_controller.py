9c5f27db4bfa2e01315c0fc707e218f5c7691573
TobyMayn <tobiasmayn@gmail.com>
2025-07-09 19:55:18 +0200

    add self param to function

--- nature-app/src/backend/controllers/token_controller.py
+++ nature-app/src/backend/controllers/token_controller.py
@@ -7,6 +7,6 @@ from services.token_service import TokenService
 
 
 class TokenController():
-    def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], 
+    def login_for_access_token(self, form_data: Annotated[OAuth2PasswordRequestForm, Depends()], 
                                session: SessionDep):
         return TokenService.login_for_access_token(form_data, session)
