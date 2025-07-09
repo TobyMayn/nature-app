58a5be8b37df09a9c7658b02f39257e0daee5909
TobyMayn <tobiasmayn@gmail.com>
2025-07-09 12:54:55 +0200

    update relative imports to absolute

--- nature-app/src/backend/main.py
+++ nature-app/src/backend/main.py
@@ -1,7 +1,7 @@
 
 from fastapi import FastAPI
 
-from .routes import layers_routers, results_routes, token_routes, users_routes
+from backend.routes import layers_routers, results_routes, token_routes, users_routes
 
 app = FastAPI(title="Nature Assessment Tool API", root_path="/api/v1", version="0.1.0")
 
