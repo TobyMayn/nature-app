9476502f711af2d93f56db88699212cef1a5f146
TobyMayn <tobiasmayn@gmail.com>
2025-07-09 19:13:01 +0200

    move models into models file

---
+++ nature-app/src/backend/models.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+from sqlmodel import Field, SQLModel
+
+
+class Users(SQLModel, table=True):
+    user_id: int = Field(primary_key=True)
+    username: str = Field(max_length=25)
+    password_hash: str = Field(max_length=30)
+    email: str | None = Field(default=None, max_length=30)
+    municipality: str | None = Field(default=None, max_length=40)
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
