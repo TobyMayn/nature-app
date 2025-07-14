2917daa0c71cb3c52d0a259e498ce2f70d7a8c39
TobyMayn <tobiasmayn@gmail.com>
2025-07-14 15:38:12 +0200

    update main.py

--- nature-app/src/backend/main.py
+++ nature-app/src/backend/main.py
@@ -1,13 +1,15 @@
 
 from fastapi import FastAPI
-from routes import layers_routers, results_routes, token_routes, users_routes
+from routes import layers_routers, results_routes, users_routes
+
+from backend.routes import auth_routes
 
 app = FastAPI(title="Nature Assessment Tool API", root_path="/api/v1", version="0.1.0")
 
 app.include_router(users_routes.router)
 app.include_router(layers_routers.router)
 app.include_router(results_routes.router)
-app.include_router(token_routes.router)
+app.include_router(auth_routes.router)
 
 
 
