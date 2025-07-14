3e3efb36fe8cc8be74fa6786ae9d94e343816797
TobyMayn <tobiasmayn@gmail.com>
2025-07-14 15:48:52 +0200

    update security

--- nature-app/src/backend/security.py
+++ nature-app/src/backend/security.py
@@ -7,4 +7,4 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/auth/login", scheme_name="JWT")
