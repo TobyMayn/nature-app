d9d98914d66b20cc12ed47ceee57e3e92eee0a9e
TobyMayn <tobiasmayn@gmail.com>
2025-07-14 15:53:34 +0200

    update main

--- nature-app/src/backend/main.py
+++ nature-app/src/backend/main.py
@@ -1,11 +1,11 @@
 
-from deps import get_current_user
+from deps import SessionDep, get_current_user
 from fastapi import FastAPI
 from routes import auth_routes, layers_routers, results_routes, users_routes
 
 app = FastAPI(title="Nature Assessment Tool API", root_path="/api/v1", version="0.1.0")
 
-app.include_router(users_routes.router, dependencies=[get_current_user])
+app.include_router(users_routes.router, dependencies=[get_current_user, SessionDep])
 app.include_router(layers_routers.router)
 app.include_router(results_routes.router)
 app.include_router(auth_routes.router)
