241f78df2bb7e537cc0d2546c6aeeccd46333ca3
TobyMayn <tobiasmayn@gmail.com>
2025-07-14 15:45:59 +0200

    Add auth to layers route

--- nature-app/src/backend/routes/layers_routers.py
+++ nature-app/src/backend/routes/layers_routers.py
@@ -1,9 +1,11 @@
-from fastapi import APIRouter
+from deps import get_current_user
+from fastapi import APIRouter, Depends
+from models import Users
 
 router = APIRouter()
 
 @router.get("/layers", tags=["layers"])
-async def get_layers():
+async def get_layers(user: Users = Depends(get_current_user)):
     return [{"text": "This is the layers path"}]
 
 @router.get("/layers/{layer_id}", tags=["layers"])
