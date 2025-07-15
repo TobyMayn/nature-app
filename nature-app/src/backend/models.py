0a1fa32907ee33882d3719becbefbfbfaf783636
TobyMayn <tobiasmayn@gmail.com>
2025-07-15 11:52:31 +0200

    add model definitions for tables in db

--- nature-app/src/backend/models.py
+++ nature-app/src/backend/models.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
-from sqlmodel import Field, SQLModel
+from shapely import Polygon
+from sqlmodel import DateTime, Field, SQLModel
 
 
 class Users(SQLModel, table=True):
@@ -9,9 +10,32 @@ class Users(SQLModel, table=True):
     email: str | None = Field(default=None, max_length=30)
     municipality: str | None = Field(default=None, max_length=40)
 
+class Location(SQLModel, table=True):
+    location_id: int | None = Field(default=None, primary_key=True)
+    polygon: Polygon
+
 class Token(BaseModel):
     access_token: str
     token_type: str
 
 class TokenData(BaseModel):
     username: str | None = None
+
+class AnalysisBody(BaseModel):
+    analysis_type: str
+    start_date: str
+    end_date: str
+    polygon: Polygon
+
+class Results(SQLModel, table=True):
+    results_id: int | None = Field(default=None, primary_key=True)
+    user_id: int = Field(foreign_key=True)
+    location_id: int = Field(foreign_key=True)
+    analysis_date: DateTime
+    analysis_type: str
+    request_params: AnalysisBody
+    status: str = Field(default="Pending")
+    requested_at: DateTime
+    completed_at: DateTime | None = Field(default=None)
+    error_message: str | None = Field(default=None)
+    result: dict | None = Field(default=None)
