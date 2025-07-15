5a7dd9d4470d6db20bbe80f477b857f2a7974d4a
TobyMayn <tobiasmayn@gmail.com>
2025-07-15 13:03:48 +0200

    add comment to db.py

--- nature-app/src/backend/database/db.py
+++ nature-app/src/backend/database/db.py
@@ -1,4 +1,5 @@
 from sqlmodel import create_engine
 
+# Create db engine instance
 mysql_url="mysql+pymysql://apiuser_test:test_password@130.226.56.134:3306/nature_app"
 engine = create_engine(mysql_url, echo=True)
