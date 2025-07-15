eefc1319e17553c432b8e2c738af70b01d9609d1
TobyMayn <tobiasmayn@gmail.com>
2025-07-15 13:04:11 +0200

    update results route

--- nature-app/src/backend/routes/results_routes.py
+++ nature-app/src/backend/routes/results_routes.py
@@ -1,11 +1,21 @@
+from controllers.results_controller import ResultsController
+from deps import SessionDep
 from fastapi import APIRouter
 
 router = APIRouter()
+results_controller = ResultsController()
+
 
 @router.get("/results", tags=["results"])
 async def get_results():
     return [{"text": "This is the results path"}]
 
+@router.post("/results/analyse", tags=["results"])
+async def analyse_area(session: SessionDep):
+    return await results_controller.analyse_area(session)
+
+
 @router.get("/results/{result_id}", tags=["results"])
 async def get_result(result_id: str):
     return [{"text": f"This is the result {result_id} path"}]
+
