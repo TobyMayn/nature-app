2ba6f72207c8aceef49a2bbd1d9f282d3714bf08
TobyMayn <tobiasmayn@gmail.com>
2025-07-15 15:56:35 +0200

    create file to handle creation and updates of results objects

---
+++ nature-app/src/backend/database/results.py
@@ -0,0 +1,48 @@
+from datetime import datetime
+
+from models import AnalysisBody, Results
+from sqlmodel import Session, select
+
+
+class ResultsAccess:
+    async def create_results(session: Session, body: AnalysisBody, user_id:int, location_id: int) -> int:
+        
+        # Create and extract fields from body object
+        analysis_date = datetime.now()
+        requested_at = body.requested_at
+        analysis_type = body.analysis_type
+
+        result = Results(user_id=user_id, location_id=location_id, analysis_date=analysis_date, 
+                         analysis_type=analysis_type, request_params=body, requested_at=requested_at)
+        
+        # Create entry of result in DB
+        session.add(result)
+        await session.commit()
+        await session.refresh(result)
+
+        return result.results_id
+
+
+    async def update_results(session: Session, result_id: int, result: dict) -> int:
+        # Retrieve correct resulta entry to update with analysis results
+        statement = select(Results).where(Results.results_id == result_id)
+        results = session.exec(statement).first()
+        if not results:
+            raise Exception #TODO: raise correct exception
+
+        results.status = "Complete"
+        results.result = result
+        results.completed_at = datetime.now()
+        error_message = result.get("error_message")
+        if error_message:
+            results.status = "Error"
+            results.error_message = error_message
+
+        session.add(results)
+        await session.commit()
+        await session.refresh(results)
+
+        return results.results_id
+        
+
+
