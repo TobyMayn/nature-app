f5aceaf94ad115ce2fea97659f388021d70b6ec7
TobyMayn <tobiasmayn@gmail.com>
2025-07-28 15:11:04 +0200

    disable algorithm service until fully functional

--- nature-app/src/backend/services/algorithm_service.py
+++ nature-app/src/backend/services/algorithm_service.py
@@ -1,11 +1,14 @@
 
 
-from algorithms.algo_factory import ConcreteAlgorithmFactory, InvalidAlgorithmException
+from algorithms.algo_factory import (  # noqa: F401
+    ConcreteAlgorithmFactory,
+    InvalidAlgorithmException,
+)
 from database.location import LocationAccess
 from database.results import ResultsAccess
-from exceptions import NoAnalysisTypeException
+from exceptions import NoAnalysisTypeException  # noqa: F401
 from image_service import ImageDownloadService
-from models import AnalysisBody, AnalysisPayload
+from models import AnalysisBody, AnalysisPayload  # noqa: F401
 from sqlmodel import Session
 
 concrete_algorithm_factory = ConcreteAlgorithmFactory()
@@ -17,40 +20,41 @@ layers = {"ortho": ["Some layers #TODO:"], "satellite": ["Some layers #TODO:"]}
 
 class AlgorithmService:
     async def create_analysis(user_id: int, session: Session, body: AnalysisBody):
-        analysis_type = body.analysis_type
-        polygon = body.polygon
-        try:
-            location_id = await db_location.create_location(session, polygon)
+        return {}
+        # analysis_type = body.analysis_type
+        # polygon = body.polygon
+        # try:
+        #     location_id = await db_location.create_location(session, polygon)
 
-            result_id = await db_results.create_results(session, body, user_id, location_id)
+        #     result_id = await db_results.create_results(session, body, user_id, location_id)
 
-        except Exception as e:
-            raise e
+        # except Exception as e:
+        #     raise e
         
-        if not analysis_type:
-            raise NoAnalysisTypeException
+        # if not analysis_type:
+        #     raise NoAnalysisTypeException
         
-        start_date = body.start_date
-        end_date = body.end_date
-        # Download photos for analysis
-        image_service.download_images_for_analysis(analysis_type=analysis_type, polygon=polygon, date_range=(start_date, end_date), layers=layers[analysis_type])
+        # start_date = body.start_date
+        # end_date = body.end_date
+        # # Download photos for analysis
+        # image_service.download_images_for_analysis(analysis_type=analysis_type, polygon=polygon, date_range=(start_date, end_date), layers=layers[analysis_type])
         
         
-        try:
-            # Get algorithm based on analysis type
-            algorithm = concrete_algorithm_factory.create_algorithm(analysis_type)
-        except InvalidAlgorithmException as e:
-            raise e
+        # try:
+        #     # Get algorithm based on analysis type
+        #     algorithm = concrete_algorithm_factory.create_algorithm(analysis_type)
+        # except InvalidAlgorithmException as e:
+        #     raise e
         
-        try:
-            # Run analysis
-            result = algorithm.predict_change()
-        except Exception as e:
-            raise e
+        # try:
+        #     # Run analysis
+        #     result = algorithm.predict_change()
+        # except Exception as e:
+        #     raise e
 
-        result_id = db_results.update_results(session, result_id, result)
+        # result_id = db_results.update_results(session, result_id, result)
 
-        return AnalysisPayload(result_id=result_id)
+        # return AnalysisPayload(result_id=result_id)
 
         
 
