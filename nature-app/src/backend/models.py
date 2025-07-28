a47bfe9365631a14e90a4fc0c8f59f644f398b18
TobyMayn <tobiasmayn@gmail.com>
2025-07-28 14:29:34 +0200

    update models

--- nature-app/src/backend/models.py
+++ nature-app/src/backend/models.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
-from pydantic import BaseModel, ConfigDict
-from sqlmodel import JSON, Field, SQLModel
+from pydantic import BaseModel
+from sqlmodel import JSON, Column, Field, SQLModel
 
 
 class Users(SQLModel, table=True):
@@ -33,15 +33,14 @@ class AnalysisPayload(BaseModel):
     result_id: int
 
 class Results(SQLModel, table=True):
-    model_config = ConfigDict(arbitrary_types_allowed=True)
     results_id: int | None = Field(default=None, primary_key=True)
     user_id: int = Field(foreign_key="users.user_id")
     location_id: int = Field(foreign_key="location.location_id")
     analysis_date: datetime
     analysis_type: str
-    request_params: JSON # This is includes the parameters from AnalysisBody. SQLModel does not support adding it as a type. So we set type to JSON.
+    request_params: dict = Field(sa_column=Column(JSON))# This is includes the parameters from AnalysisBody. SQLModel does not support adding it as a type. So we set type to JSON.
     status: str = Field(default="Pending")
     requested_at: datetime
     completed_at: datetime | None = Field(default=None)
     error_message: str | None = Field(default=None)
-    result: JSON | None = Field(default=None)
+    result: dict | None = Field(default=None, sa_column=Column(JSON))
