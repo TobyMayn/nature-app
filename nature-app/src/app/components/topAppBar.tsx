250b0b799d8a7580eae699f251a9299c62414b27
TobyMayn <tobiasmayn@gmail.com>
2025-07-29 15:09:44 +0200

    update top app bar with functionality to logout a user

--- nature-app/src/app/components/topAppBar.tsx
+++ nature-app/src/app/components/topAppBar.tsx
@@ -1,10 +1,23 @@
+"use client";
+
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import { signOut } from 'next-auth/react';
+import { useAuth } from '../contexts/auth-context';
+import { useRouter } from 'next/navigation';
 
 export default function ButtonAppBar() {
+  const { isAuthenticated } = useAuth();
+  const router = useRouter();
+
+  const handleLogout = async () => {
+    await signOut({ redirect: false });
+    router.push('/login');
+  };
+
   return (
     <Box sx={{ flexGrow: 1}}>
       <AppBar position="static" sx={{bgcolor: "#136100",}}>
@@ -12,7 +25,11 @@ export default function ButtonAppBar() {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1}}>
             Nature Assessment Tool
           </Typography>
-          <Button color="inherit">Login</Button>
+          {isAuthenticated && (
+            <Button color="inherit" onClick={handleLogout}>
+              Logout
+            </Button>
+          )}
         </Toolbar>
       </AppBar>
     </Box>
