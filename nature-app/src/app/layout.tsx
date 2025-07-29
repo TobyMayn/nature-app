0920eed9ccda5cc496f161f55889ce554a5f3b6b
TobyMayn <tobiasmayn@gmail.com>
2025-07-29 15:06:59 +0200

    add session and auth provider to layout.tsx

--- nature-app/src/app/layout.tsx
+++ nature-app/src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { SessionProvider } from "next-auth/react";
+import { AuthProvider } from "./contexts/auth-context";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,7 +29,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <SessionProvider>
+          <AuthProvider>
+            {children}
+          </AuthProvider>
+        </SessionProvider>
       </body>
     </html>
   );
