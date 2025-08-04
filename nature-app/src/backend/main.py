b7a286c1044989ba28b67320cb45e177bceb881b
TobyMayn <tobiasmayn@gmail.com>
2025-08-04 10:40:43 +0200

    setup CORS to be able to use client app from localhost

--- nature-app/src/backend/main.py
+++ nature-app/src/backend/main.py
@@ -1,8 +1,17 @@
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes import auth_routes, layers_routers, results_routes, users_routes
 
 app = FastAPI(title="Nature Assessment Tool API", root_path="/api/v1", version="0.1.0")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(users_routes.router)
 app.include_router(layers_routers.router)
