a61e1bfa1430e5576116333cd397df77297dc288
TobyMayn <tobiasmayn@gmail.com>
2025-08-09 13:55:39 +0200

    update basepath to frontend

--- nature-app/next.config.ts
+++ nature-app/next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: '/nature',
+  assetPrefix: '/nature',
 };
 
 export default nextConfig;
