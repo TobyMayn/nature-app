0f27b3bed945f90e990892b710d46fb52c3361a6
TobyMayn <tobiasmayn@gmail.com>
2026-02-04 16:29:54 +0100

    quick fix cors to run on wildcards

--- nature-app/src/backend/main.py
+++ nature-app/src/backend/main.py
@@ -13,18 +13,16 @@ load_dotenv()
 API_TITLE = os.getenv("API_TITLE")
 API_ROOT_PATH = os.getenv("API_ROOT_PATH")
 API_VERSION = os.getenv("API_VERSION")
-CORS_ALLOW_ORIGINS = os.getenv("CORS_ALLOW_ORIGINS")
-CORS_ALLOW_METHODS = os.getenv("CORS_ALLOW_METHODS")
-CORS_ALLOW_HEADERS = os.getenv("CORS_ALLOW_HEADERS")
+CORS_ALLOW_ORIGINS = ["http://localhost", "http://localhost:8080"]
 
 app = FastAPI(title=API_TITLE, root_path=API_ROOT_PATH, version=API_VERSION)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[CORS_ALLOW_ORIGINS],
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=[CORS_ALLOW_METHODS],
-    allow_headers=[CORS_ALLOW_HEADERS],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(users_routes.router)
