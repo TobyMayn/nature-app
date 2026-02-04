10ff19d83815d2aad4d919677316468e2283511a
TobyMayn <tobiasmayn@gmail.com>
2026-02-04 16:30:23 +0100

    remove echo true from db connection

--- nature-app/src/backend/database/db.py
+++ nature-app/src/backend/database/db.py
@@ -15,4 +15,4 @@ DB_NAME = os.getenv("DB_NAME")
 DB_ECHO = os.getenv("DB_ECHO")
 
 mysql_url = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-engine = create_engine(mysql_url, echo=DB_ECHO)
+engine = create_engine(mysql_url)
